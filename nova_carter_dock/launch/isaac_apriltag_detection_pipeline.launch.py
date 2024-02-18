b2981f52d065135d943599c691c806988c08688c
Steve Macenski <stevenmacenski@gmail.com>
2024-02-17 17:25:09 -0800

    an initial launch file for nova carter dock

---
+++ nova_carter_dock/launch/isaac_apriltag_detection_pipeline.launch.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2024 Open Navigation LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import launch
+from launch_ros.actions import ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    apriltag_node = ComposableNode(
+        package='isaac_ros_apriltag',
+        plugin='nvidia::isaac_ros::apriltag::AprilTagNode',
+        name='apriltag',
+        remappings=[
+            ('image', '/hawk_front/left/image_raw'),        # /owl_front/left/image_raw
+            ('camera_info', '/hawk_front/left/camerainfo')  # /owl_front/left/camerainfo
+        ],
+        parameters=[{'size': 0.1524,  # 6 inches
+                     'max_tags': 4,
+                     'tile_size': 4}])
+
+    apriltag_container = ComposableNodeContainer(
+        package='rclcpp_components',
+        name='apriltag_container',
+        namespace='',
+        executable='component_container_mt',
+        composable_node_descriptions=[
+            apriltag_node,
+        ],
+        output='screen'
+    )
+
+    dock_pose_publisher = Node(
+        package='nova_caster_dock',
+        executable='dock_pose_publisher',
+        name='dock_pose_publisher',
+        parameters=[{'use_first_detection': False}],
+    )
+
+    return launch.LaunchDescription([apriltag_container, dock_pose_publisher])
---
+++ nova_carter_dock/src/dock_pose_publisher.cpp
@@ -0,0 +1,77 @@
+// Copyright (c) 2024 Open Navigation LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "isaac_ros_apriltag_interfaces/msg/april_tag_detection_array.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+
+using std::placeholders::_1;
+
+class DockPosePublisher : public rclcpp::Node
+{
+  public:
+    DockPosePublisher()
+    : Node("dock_pose_publisher")
+    {
+      subscription_ = this->create_subscription<isaac_ros_apriltag_interfaces::msg::AprilTagDetectionArray>(
+      "tag_detections", 10, std::bind(&DockPosePublisher::detectionCallback, this, _1));
+      publisher_ = this->create_publisher<geometry_msgs::msg::PoseStamped>("detected_dock_pose", 10);
+      
+      // If you don't expect multiple detections in a scene, use the only apriltag
+      use_first_detection_ = this->declare_parameter("use_first_detection", true);
+
+      // If you expect multiple detections in a scene, specify which you want to use
+      // (default here is included in media/ directory)
+      dock_tag_family_ = this->declare_parameter("dock_tag_family", "tag36h11");
+      dock_tag_id_ = this->declare_parameter("dock_tag_id", 585);
+    }
+
+  private:
+    void detectionCallback(const isaac_ros_apriltag_interfaces::msg::AprilTagDetectionArray::SharedPtr msg)
+    {
+      geometry_msgs::msg::PoseStamped p;
+      for (unsigned int i = 0; i != msg->detections.size(); i++) {
+        if (!use_first_detection_) {
+          if (msg->detections[i].family == dock_tag_family_ && msg->detections[i].id == dock_tag_id_) {
+            p.header = msg->detections[i].pose.header;
+            p.pose = msg->detections[i].pose.pose.pose;
+            publisher_->publish(p);
+            return;
+          }
+        } else {
+          // Use the first detection found
+          p.header = msg->detections[0].pose.header;
+          p.pose = msg->detections[0].pose.pose.pose;
+          publisher_->publish(p);
+          return;
+        }
+      }
+    }
+
+    std::string dock_tag_family_;
+    int dock_tag_id_;
+    bool use_first_detection_;
+    rclcpp::Subscription<isaac_ros_apriltag_interfaces::msg::AprilTagDetectionArray>::SharedPtr subscription_;
+    rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr publisher_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<DockPosePublisher>());
+  rclcpp::shutdown();
+  return 0;
+}
